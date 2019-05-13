109db6610f578ced13255a654fd73d37c3f070c2
Ismael Ezequiel <kell.trollhammaren@hotmail.com>
2019-05-13 20:17:17 -0300

    Updates

---
+++ precache-manifest.d2ea155bf1b4e418704a3f7d31892402.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86090f563dc7407d1a50dcce132bec37",
+    "url": "/pumplabs/index.html"
+  },
+  {
+    "revision": "b132171d4edf6158b6e0",
+    "url": "/pumplabs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/pumplabs/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "b132171d4edf6158b6e0",
+    "url": "/pumplabs/static/js/main.18bea313.chunk.js"
+  },
+  {
+    "revision": "427bb5a3edb8c0efd5a8",
+    "url": "/pumplabs/static/js/runtime~main.f7d40234.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pumplabs/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eeb0279608e534000037d45c31ffe8ef.js"
+  "/pumplabs/precache-manifest.d2ea155bf1b4e418704a3f7d31892402.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pumplabs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
