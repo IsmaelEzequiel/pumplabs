aa67a6683f5ecb8d3bb4fb2299523b3db1613d7e
Ismael Ezequiel <kell.trollhammaren@hotmail.com>
2019-05-13 21:23:45 -0300

    firebase env names

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,13 @@
 import * from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZZ-c7v7VvkQXeDyWBuCanvUjZGh6YEdU",
-  authDomain: "pumps-lab.firebaseapp.com",
-  databaseURL: "https://pumps-lab.firebaseio.com",
-  projectId: "pumps-lab",
-  storageBucket: "pumps-lab.appspot.com",
-  messagingSenderId: "1096152642483",
-  appId: "1:1096152642483:web:db9a4ad133da5fed"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
