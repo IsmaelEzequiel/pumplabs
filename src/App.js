2b8e5118a62e813d486823b18b7bbc65900cb051
Ismael Ezequiel <kell.trollhammaren@hotmail.com>
2019-05-13 21:14:19 -0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/Navigation'
+
+import * as ROUTES from './constants/routes'
+
+import SignInPage from './components/Pages/SignInPage'
+import SignUpPage from './components/Pages/SignUpPage'
+import AccountPage from './components/Pages/AccountPage'
+import AdminPage from './components/Pages/AdminPage'
+import HomePage from './components/Pages/HomePage'
+import LandingPage from './components/Pages/LandingPage'
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <Navigation />
+      <Route exact path={ROUTES.HOME} component={LandingPage} />
+      <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route exact path={ROUTES.ADMIN} component={AdminPage} />
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route exact path={ROUTES.ACCOUNT} component={AccountPage} />
+    </Router>
   );
 }
 
---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import * from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDZZ-c7v7VvkQXeDyWBuCanvUjZGh6YEdU",
+  authDomain: "pumps-lab.firebaseapp.com",
+  databaseURL: "https://pumps-lab.firebaseio.com",
+  projectId: "pumps-lab",
+  storageBucket: "pumps-lab.appspot.com",
+  messagingSenderId: "1096152642483",
+  appId: "1:1096152642483:web:db9a4ad133da5fed"
+}
+
+firebase.initializeApp(firebaseConfig)
